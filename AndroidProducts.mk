0b12e6d6917f9ab0a2c491ef9c2ef56f80dd1b28
Nomanali3468 <nomanali3468@users.noreply.github.com>
2026-04-11 01:51:44 +0400

    Add twrp_LG6n.mk product config

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_LG6n.mk
+    $(LOCAL_DIR)/omni_LG6n.mk \
+    $(LOCAL_DIR)/twrp_LG6n.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_LG6n-eng \
-    omni_LG6n-userdebug
+    omni_LG6n-userdebug \
+    twrp_LG6n-eng
